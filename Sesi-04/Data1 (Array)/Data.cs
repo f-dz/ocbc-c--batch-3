5b5e47e52ab94c1933ed17ab72eb4aa4bb5573d7
f-dz <fatinzakiya@student.undip.ac.id>
2022-03-22 15:27:52 +0700

    Third Commit

---
+++ Sesi-04/Data1 (Array)/Data.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Data
+{
+    public static void Main()
+    {
+        int[] angka = new int[5];
+        string[] aNama = new string[] {"Joni", "Meri", "David"};
+        object[] aObjek = { 20, 33, "Lorem ipsum", DateTime.Now, true, 'D'};
+    }
+}
